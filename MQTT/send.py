610f5e2da96f876eda756c15ecd971fa8f988536
Lee <leeuklee@gmail.com>
2022-08-01 00:51:57 +0200

    updated

--- MQTT/send.py
+++ MQTT/send.py
@@ -10,7 +10,8 @@ client.connect(host=mq_host)
 
 # while True:
 colour = random.choice(["blue", "green", "red"])
-message = '{{"colour" : "{}", "value" : 1}}'.format(colour)
+value = random.choice([0, 1])
+message = '{{"colour" : "{}", "value" : {} }}'.format(colour, value)
 client.publish(queue_topic, message, retain=True)
 print("Just published {} to topic {}".format(message, queue_topic))
 client.disconnect()
